b929d670b08d3c091e97a20863f95c9759081d0c
ttymsd <ttymsd@gmail.com>
2018-06-04 09:11:51 +0900

    fix

--- create_properties.sh
+++ create_properties.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-echo $ANDROID_LICENSES | base64 --decode > /usr/local/android-sdk-linux/licenses
+mkdir -p $ANDROID_HOME/licenses
+echo $ANDROID_LICENSES | base64 --decode > /usr/local/android-sdk-linux/licenses/android-sdk-license
 echo $LOCAL_PROPERTIES | base64 --decode  > local.properties
