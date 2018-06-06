5c7ce41bac305f2d8c6a7edf15ab0608a934bc0a
Tetsuya Masuda <ttymsd@gmail.com>
2018-06-07 06:09:07 +0900

    Fix ci (#11)
    create android license before build
    fix typo circle ci config dir

--- create_properties.sh
+++ create_properties.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+mkdir -p $ANDROID_HOME/licenses
+echo $ANDROID_SDK_LICENSE > /usr/local/android-sdk-linux/licenses/android-sdk-license
 echo $LOCAL_PROPERTIES | base64 --decode  > local.properties
